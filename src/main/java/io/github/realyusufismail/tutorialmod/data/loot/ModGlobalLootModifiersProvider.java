3ac2515a5885a67d19d78c361ecd688a0ee9cd5e
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2024-06-19 16:58:21 +0100

    modifed global loot

--- src/main/java/io/github/realyusufismail/tutorialmod/data/loot/ModGlobalLootModifiersProvider.java
+++ src/main/java/io/github/realyusufismail/tutorialmod/data/loot/ModGlobalLootModifiersProvider.java
@@ -15,6 +15,7 @@ import net.neoforged.neoforge.common.loot.LootTableIdCondition;
 
 import java.util.concurrent.CompletableFuture;
 
+// see https://github.com/Luohuayu/CatServer/blob/1c92118fcca69ffac97a48c8e1f6e1bb861b41d1/src/main/java/org/bukkit/loot/LootTables.java#L71 for some loot tables
 public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
     public ModGlobalLootModifiersProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, TutorialMod.MOD_ID);
@@ -23,15 +24,14 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
     @Override
     protected void start() {
         // Add your loot modifiers here
-        add("example_item_from_grass", new AddItemModifier(new LootItemCondition[]{
-                LootItemBlockStatePropertyCondition.hasBlockStateProperties(Blocks.GRASS_BLOCK).build(),
-                // 35% chance
+        add("example_item_from_igloo_chest", new AddItemModifier(new LootItemCondition[]{
+                LootTableIdCondition.builder(ResourceLocation.parse("chests/igloo_chest")).build(),
+                // 35% chance to get example item from igloo chest
                 LootItemRandomChanceCondition.randomChance(0.35f).build()},
                 ItemInit.EXAMPLE_ITEM.get()));
 
-        add("example_item_from_creeper", new AddItemModifier(new LootItemCondition[]{
-                LootItemBlockStatePropertyCondition.hasBlockStateProperties(Blocks.GRASS_BLOCK).build(),
-                LootTableIdCondition.builder(ResourceLocation.parse("entities/creeper")).build()},
+        add("example_item_from_iron_golem", new AddItemModifier(new LootItemCondition[]{
+                LootTableIdCondition.builder(ResourceLocation.parse("entities/iron_golem")).build()},
                 ItemInit.EXAMPLE_ITEM.get()));
     }
 }
